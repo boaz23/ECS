60b65748572b6bb8100e8c3019f39a6261ba15bb
Boaz Rozovski <boazrozo@post.bgu.ac.il>
2019-12-13 18:34:03 +0200

    Implemented the rest of the controls

--- HackMachine/Machine/CPU16.cs
+++ HackMachine/Machine/CPU16.cs
@@ -12,6 +12,18 @@ namespace Machine
         //this "enum" defines the different control bits names
         public const int J3 = 0, J2 = 1, J1 = 2, D3 = 3, D2 = 4, D1 = 5, C6 = 6, C5 = 7, C4 = 8, C3 = 9, C2 = 10, C1 = 11, A = 12, X2 = 13, X1 = 14, Type = 15;
 
+        private const int SINGLE_BIT_SIZE = 1;
+        private const int JUMP_CONTROLS_COUNT = 3;
+
+        private const int NJ  = 0b000;
+        private const int JGT = 0b001;
+        private const int JEQ = 0b010;
+        private const int JGE = 0b011;
+        private const int JLT = 0b100;
+        private const int JNE = 0b101;
+        private const int JLE = 0b110;
+        private const int JMP = 0b111;
+
         public int Size { get; private set; }
 
         //CPU inputs
@@ -78,65 +90,225 @@ namespace Machine
 
         //add here components to implement the control unit 
         private BitwiseMultiwayMux m_gJumpMux;//an example of a control unit compnent - a mux that controls whether a jump is made
-        private WireSet m_cInstructionResetWs;
+
+        private WireSet m_wsCInstructionReset;
         private BitwiseAndGate m_gInstructionAnd;
         private BitwiseMux m_gInstructionMux;
+
         private NotGate m_gALoadInstructionNot;
         private OrGate m_gALoadOr;
+
+        private Wire[] m_wJump;
+
+        private NotGate m_gJGT_Not;
+        private NotGate m_gJGE_Not;
+        private NotGate m_gJNE_Not;
+        private OrGate m_gJLE_Or;
 
         private void ConnectControls()
         {
             //1. connect control of mux 1 (selects entrance to register A)
-            m_gAMux.ConnectControl(Instruction[Instruction.Size - 1]);
+            ChooseARegisterInput();
 
             //2. connect control to mux 2 (selects A or M entrance to the ALU)
-            m_gMAMux.ConnectControl(Instruction[12]);
+            ChooseALU_AorM_Input();
 
             //3. consider all instruction bits only if C type instruction (MSB of instruction is 1)
-            m_cInstructionResetWs = new WireSet(Size);
-            int[] cInstructionResetWsValues = new int[] { 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, };
+            FilterInstructionBits();
+
+            //4. connect ALU control bits
+            ConnectALUControl();
+
+            //5. connect control to register D (very simple)
+            ConnectDRegisterLoad();
+
+            //6. connect control to register A (a bit more complicated)
+            ConnectARegisterLoad();
+
+            //7. connect control to MemoryWrite
+            ConnectMemoryWrite();
+
+            //8. create inputs for jump mux
+            ConnectJumpWires();
+
+            //9. connect jump mux (this is the most complicated part)
+            ConnectJumpMux();
+
+            //10. connect PC load control
+            ConnectPCLoad();
+        }
+
+        private void ChooseARegisterInput()
+        {
+            m_gAMux.ConnectControl(Instruction[Type]);
+        }
+
+        private void ChooseALU_AorM_Input()
+        {
+            m_gMAMux.ConnectControl(Instruction[A]);
+        }
+
+        private void FilterInstructionBits()
+        {
+            m_wsCInstructionReset = new WireSet(Size);
+            int[] cInstructionResetWsValues = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
             for (int i = 0; i < Size; i++)
             {
-                m_cInstructionResetWs[i].Value = cInstructionResetWsValues[i];
+                m_wsCInstructionReset[i].Value = cInstructionResetWsValues[i];
             }
 
             m_gInstructionAnd = new BitwiseAndGate(Size);
             m_gInstructionAnd.Input1.ConnectInput(Instruction);
-            m_gInstructionAnd.Input2.ConnectInput(m_cInstructionResetWs);
+            m_gInstructionAnd.Input2.ConnectInput(m_wsCInstructionReset);
+
             m_gInstructionMux = new BitwiseMux(Size);
             m_gInstructionMux.Input1.ConnectInput(m_gInstructionAnd.Output);
             m_gInstructionMux.Input2.ConnectInput(Instruction);
-            m_gInstructionMux.ConnectControl(Instruction[Instruction.Size - 1]);
+            m_gInstructionMux.ConnectControl(Instruction[Type]);
+        }
 
-            //4. connect ALU control bits
-            m_gALU.ZeroX.ConnectInput(m_gInstructionMux.Output[11]);
-            m_gALU.NotX.ConnectInput(m_gInstructionMux.Output[10]);
-            m_gALU.ZeroY.ConnectInput(m_gInstructionMux.Output[9]);
-            m_gALU.NotY.ConnectInput(m_gInstructionMux.Output[8]);
-            m_gALU.F.ConnectInput(m_gInstructionMux.Output[7]);
-            m_gALU.NotOutput.ConnectInput(m_gInstructionMux.Output[6]);
+        private void ConnectALUControl()
+        {
+            m_gALU.ZeroX.ConnectInput(Instruction[C1]);
+            m_gALU.NotX.ConnectInput(Instruction[C2]);
+            m_gALU.ZeroY.ConnectInput(Instruction[C3]);
+            m_gALU.NotY.ConnectInput(Instruction[C4]);
+            m_gALU.F.ConnectInput(Instruction[C5]);
+            m_gALU.NotOutput.ConnectInput(Instruction[C6]);
+        }
 
-            //5. connect control to register D (very simple)
-            m_rD.Load.ConnectInput(m_gInstructionMux.Output[4]);
+        private void ConnectDRegisterLoad()
+        {
+            m_rD.Load.ConnectInput(m_gInstructionMux.Output[D2]);
+        }
 
-            //6. connect control to register A (a bit more complicated)
+        private void ConnectARegisterLoad()
+        {
             m_gALoadInstructionNot = new NotGate();
-            m_gALoadInstructionNot.Input.ConnectInput(Instruction[Instruction.Size - 1]);
+            m_gALoadInstructionNot.Input.ConnectInput(Instruction[Type]);
+
             m_gALoadOr = new OrGate();
             m_gALoadOr.Input1.ConnectInput(m_gALoadInstructionNot.Output);
-            m_gALoadOr.Input2.ConnectInput(Instruction[5]);
+            m_gALoadOr.Input2.ConnectInput(Instruction[D1]);
+
             m_rA.Load.ConnectInput(m_gALoadOr.Output);
+        }
 
-            //7. connect control to MemoryWrite
-            MemoryWrite.ConnectInput(Instruction[3]);
+        private void ConnectMemoryWrite()
+        {
+            MemoryWrite.ConnectInput(m_gInstructionMux.Output[D3]);
+        }
 
-            //8. create inputs for jump mux
+        private void ConnectJumpWires()
+        {
+            InitializeJumpWireSets();
+            ConnectJumpWire_NJ();
+            ConnectJumpWire_JGT();
+            ConnectJumpWire_JEQ();
+            ConnectJumpWire_JGE();
+            ConnectJumpWire_JLT();
+            ConnectJumpWire_JNE();
+            ConnectJumpWire_JLE();
+            ConnectJumpWire_JMP();
+        }
 
+        private void InitializeJumpWireSets()
+        {
+            m_wJump = new Wire[(int)Math.Pow(2, JUMP_CONTROLS_COUNT)];
+            for (int i = 0; i < m_wJump.Length; i++)
+            {
+                m_wJump[i] = new Wire();
+            }
+        }
 
-            //9. connect jump mux (this is the most complicated part)
-            m_gJumpMux = new BitwiseMultiwayMux(1, 3);
-            
-            //10. connect PC load control
+        private void ConnectJumpWire_NJ()
+        {
+            m_wJump[NJ].Value = 0;
+        }
+
+        private void ConnectJumpWire_JGT()
+        {
+            m_gJLE_Or = new OrGate();
+            m_gJLE_Or.Input1.ConnectInput(m_gALU.Zero);
+            m_gJLE_Or.Input2.ConnectInput(m_gALU.Negative);
+
+            m_gJGT_Not = new NotGate();
+            m_gJGT_Not.ConnectInput(m_gJLE_Or.Output);
+
+            m_wJump[JGT].ConnectInput(m_gJGT_Not.Output);
+        }
+
+        private void ConnectJumpWire_JEQ()
+        {
+            m_wJump[JEQ].ConnectInput(m_gALU.Zero);
+        }
+
+        private void ConnectJumpWire_JGE()
+        {
+            m_gJGE_Not = new NotGate();
+            m_gJGE_Not.Input.ConnectInput(m_gALU.Negative);
+
+            m_wJump[JGE].ConnectInput(m_gJGE_Not.Output);
+        }
+
+        private void ConnectJumpWire_JLT()
+        {
+            m_wJump[JLT].ConnectInput(m_gALU.Negative);
+        }
+
+        private void ConnectJumpWire_JNE()
+        {
+            m_gJNE_Not = new NotGate();
+            m_gJNE_Not.Input.ConnectInput(m_gALU.Zero);
+
+            m_wJump[JNE].ConnectInput(m_gJNE_Not.Output);
+        }
+
+        private void ConnectJumpWire_JLE()
+        {
+            m_wJump[JLE].ConnectInput(m_gJLE_Or.Output);
+        }
+
+        private void ConnectJumpWire_JMP()
+        {
+            m_wJump[JMP].Value = 1;
+        }
+
+        private void ConnectJumpMux()
+        {
+            InitializeJumpMux();
+            ConnectJumpWiresToMux();
+            ConnectJumpMuxControl();
+        }
+
+        private void InitializeJumpMux()
+        {
+            m_gJumpMux = new BitwiseMultiwayMux(SINGLE_BIT_SIZE, JUMP_CONTROLS_COUNT);
+        }
+
+        private void ConnectJumpWiresToMux()
+        {
+            for (int iJump = 0; iJump < m_wJump.Length; iJump++)
+            {
+                ConnectJumpWireToMux(iJump);
+            }
+        }
+
+        private void ConnectJumpWireToMux(int iJump)
+        {
+            m_gJumpMux.Inputs[iJump][0].ConnectInput(m_wJump[iJump]);
+        }
+
+        private void ConnectJumpMuxControl()
+        {
+            m_gJumpMux.Control[0].ConnectInput(m_gInstructionMux.Output[J3]);
+            m_gJumpMux.Control[1].ConnectInput(m_gInstructionMux.Output[J2]);
+            m_gJumpMux.Control[2].ConnectInput(m_gInstructionMux.Output[J1]);
+        }
+
+        private void ConnectPCLoad()
+        {
+            m_rPC.ConnectLoad(m_gJumpMux.Output[0]);
         }
 
         public override string ToString()
@@ -168,5 +340,8 @@ namespace Machine
             Console.WriteLine("outM=" + MemoryOutput);
             Console.WriteLine("addM=" + MemoryAddress);
         }
+
+        public int PC => m_rPC.Output.Value;
+        public int AReg => m_rA.Output.Value;
     }
 }
--- HackMachine/Machine/Program.cs
+++ HackMachine/Machine/Program.cs
@@ -14,17 +14,24 @@ namespace Machine
         [STAThread]
         static void Main(string[] args)
         {
-            const string FilePath = @"D:\Programming - University\Electronic Computing Systems\HackMachine\Assembly examples\Copy1To0.hack";
+            const string FilePath = @"Assembly examples\Product.hack";
 
             Machine16 machine = new Machine16(false, false);
             machine.Code.LoadFromFile(FilePath);
-            int linesCount = File.ReadAllLines(FilePath).Length;
+            Func<bool>  whileCondition = GetRunningCondition(FilePath, machine);
 
-            machine.Data[0] = 100;
-            machine.Data[1] = 15;
+            machine.Data[0] = 9;
+            machine.Data[1] = 7;
+            for (int i = 100; i <= 200; i++)
+            {
+                machine.Data[i] = 5 + i;
+            }
+
             DateTime dtStart = DateTime.Now;
             machine.Reset();
-            for (int i = 0; i < linesCount; i++)
+
+            int consecutiveZeros = 0;
+            while (whileCondition() && !ReadConsecutiveZeros(machine.Code, machine.CPU.PC, ref consecutiveZeros))
             {
                 machine.CPU.PrintState();
                 Console.WriteLine();
@@ -32,10 +39,64 @@ namespace Machine
                 Clock.ClockUp();
             }
 
-
-            Console.WriteLine(machine.Data[0]);
+            Console.WriteLine(machine.Data[2]);
             Console.WriteLine("Done " + (DateTime.Now - dtStart).TotalSeconds);
             Console.ReadLine();
+        }
+
+        private static Func<bool> GetRunningCondition(string FilePath, Machine16 machine)
+        {
+            Func<bool> whileCondition;
+            int lastInstruction = FindLastInstruction(machine.Code);
+            bool isLastInstructionJump = IsLastInstructionJump(machine.Code, lastInstruction);
+            if (isLastInstructionJump)
+            {
+                whileCondition = () => !(machine.CPU.PC == lastInstruction && machine.CPU.AReg == lastInstruction - 1);
+            }
+            else
+            {
+                whileCondition = () => machine.CPU.PC <= lastInstruction;
+            }
+
+            return whileCondition;
+        }
+
+        private static bool IsLastInstructionJump(Memory code, int lastInstruction)
+        {
+            int instruction = code[lastInstruction] & 0b1001111111111111;
+            return (instruction & 0b1000000000000111) == 0b1000000000000111;
+        }
+
+        private static int FindLastInstruction(Memory code)
+        {
+            int consecutiveZeros = 0;
+            for (int i = 0; i < code.Size; i++)
+            {
+                if (ReadConsecutiveZeros(code, i, ref consecutiveZeros))
+                {
+                    return i - consecutiveZeros;
+                }
+            }
+
+            return code.Size - 1;
+        }
+
+        private static bool ReadConsecutiveZeros(Memory code, int i, ref int counter)
+        {
+            if (code[i] == 0)
+            {
+                counter++;
+                if (counter == 10)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                counter = 0;
+            }
+
+            return false;
         }
     }
 }
